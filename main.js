6334efa4a4d0bd21c6437d5d27a34b4d801e4721
Harish0411 <hariharanramasamy98@gmail.com>
2023-02-16 20:34:10 +0530

    vowels or not added.

---
+++ main.js
@@ -0,0 +1,11 @@
+let ch = prompt("Enter any alphabet");
+
+if((ch=="a") || (ch=="e") || (ch=="i") || (ch=="o") || (ch=="u")){
+     document.write(`${ch} is a vowel.`);
+}
+else if((ch=="A") || (ch=="E") || (ch=="I") || (ch=="O") || (ch=="U")){
+     document.write(`${ch} is a vowel.`);
+}
+else{
+      document.write(`${ch} is a consonant.`);
+	 }
